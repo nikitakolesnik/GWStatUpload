88ee232313c61e16f05db97d8c8360323d51d9fb
Nikita Kolesnik <nikitakolesnik@yahoo.com>
2023-09-24 02:52:47 -0700

    idk

--- Server/StatServer/DTOs/MatchEntryDTO.cs
+++ Server/StatServer/DTOs/MatchEntryDTO.cs
@@ -1,7 +1,26 @@
-﻿namespace StatServer.DTOs
+﻿using System.Text.Json.Serialization;
+
+namespace StatServer.DTOs
 {
 	public class MatchEntryDTO
 	{
+		public List<PlayerDTO> Players { get; set; }
+	}
 
+	public class PlayerDTO
+	{
+		public List<SkillDTO> Skills { get; set; }
+	}
+
+	public class SkillDTO
+	{
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("skill_id")]
+		public int SkillId { get; set; }
+
+		[JsonPropertyName("count")]
+		public int Count { get; set; }
 	}
 }
